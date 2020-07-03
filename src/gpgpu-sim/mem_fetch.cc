f2c4389fd4afa7caba86660343642f4a2f3e4f27
prdalmia <pdalmia@wisc.edu>
2020-07-03 18:27:24 -0500

    message

--- src/gpgpu-sim/mem_fetch.cc
+++ src/gpgpu-sim/mem_fetch.cc
@@ -144,7 +144,7 @@ unsigned mem_fetch::get_num_flits(bool simt_to_mem){
 	if( isatomic() || (simt_to_mem && get_is_write()) || !(simt_to_mem || get_is_write()) ){
 		sz = size();
         if (isatomic() && isatomicdone() && simt_to_mem){
-            //sz = sz*4;
+            sz = sz*4;
             printf("the flit  size for atomic is %d\n", (sz/icnt_flit_size) + ( (sz % icnt_flit_size)? 1:0));        
             }
     }
