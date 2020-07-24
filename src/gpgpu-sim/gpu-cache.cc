b9061a0899e9a637f91ca7e5564c773ca86bef1d
prdalmia <pdalmia@wisc.edu>
2020-07-24 00:07:58 -0500

    message

--- src/gpgpu-sim/gpu-cache.cc
+++ src/gpgpu-sim/gpu-cache.cc
@@ -1183,7 +1183,8 @@ bool baseline_cache::waiting_for_fill( mem_fetch *mf ){
 
 /// Checks if mf is waiting to be filled by lower memory level
 void  baseline_cache::invalidate( unsigned m_sid, const memory_config* mem_config){
-  std::vector<new_addr_type> flush_queue = m_tag_array->invalidate();
+  std::vector<new_addr_type> flush_queue = m_tag_array->invalidate()
+  std::list<cache_event> events;;
   for (unsigned i=0; i < flush_queue.size(); i++){
    mem_access_t access( GLOBAL_ACC_R, flush_queue[i], WRITE_PACKET_SIZE, 1 );
     mem_fetch *mf = new mem_fetch( access, 
@@ -1197,7 +1198,7 @@ void  baseline_cache::invalidate( unsigned m_sid, const memory_config* mem_confi
            
            
            
-           m_icnt->push(mf);
+           send_write_request(mf, cache_event(WRITE_REQUEST_SENT), time, events);
 }
 void baseline_cache::print(FILE *fp, unsigned &accesses, unsigned &misses) const{
     fprintf( fp, "Cache %s:\t", m_name.c_str() );
