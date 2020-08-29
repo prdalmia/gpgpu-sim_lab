9b3f7f332324853fb7e03e223e10ee38c5e69654
prdalmia <pdalmia@wisc.edu>
2020-08-29 15:50:26 -0500

    message

--- src/gpgpu-sim/shader.cc
+++ src/gpgpu-sim/shader.cc
@@ -1688,7 +1688,7 @@ void ldst_unit::L1_latency_queue_cycle()
 		    mem_fetch* mf_next = l1_latency_queue[0];
 			std::list<cache_event> events;
 			enum cache_request_status status = m_L1D->access(mf_next->get_addr(),mf_next,gpu_sim_cycle+gpu_tot_sim_cycle,events);
-            if(mf_next->isatomic() == true){
+            if(mf_next->isatomic() == true || mf_next->get_sid() == 4){
         printf("Core %d has a recieved an atomic transaction for address %x\n", mf_next->get_sid(), mf_next->get_addr());
     }
 		   bool write_sent = was_write_sent(events);
