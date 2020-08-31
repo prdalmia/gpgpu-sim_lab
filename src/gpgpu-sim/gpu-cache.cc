8c9168a663c0365925ab4acb5faf0af648f405f4
prdalmia <pdalmia@wisc.edu>
2020-08-31 12:00:15 -0500

    message

--- src/gpgpu-sim/gpu-cache.cc
+++ src/gpgpu-sim/gpu-cache.cc
@@ -1883,7 +1883,7 @@ l1_cache::evict(   mem_fetch *mf,
     enum cache_request_status status = m_tag_array->probe(block_addr,cache_index,mf);
      
      if(mf->get_sid() == 4){
-        printf("mshr_hit and status are %d and %d respectively for adddress %x", mshr_hit, status, mf->get_addr());
+        printf("mshr_hit and status are %d and %d respectively for adddress %x\n", mshr_hit, status, mf->get_addr());
         }
     if ( !mshr_hit && status == HIT ) { // DO WE HAVE TO INCLUDE SECTOR MISS?  
 
