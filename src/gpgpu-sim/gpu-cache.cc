a966a2594ef179c572f96159502d6182c89c07d4
prdalmia <pdalmia@wisc.edu>
2020-09-03 16:01:37 -0500

    message

--- src/gpgpu-sim/gpu-cache.cc
+++ src/gpgpu-sim/gpu-cache.cc
@@ -1901,9 +1901,9 @@ l1_cache::evict(   mem_fetch *mf,
 	cache_block_t* block = m_tag_array->get_block(cache_index);
     mf->set_type(INVALIDATION_RESPONSE);
 	send_write_request(mf, cache_event(WRITE_REQUEST_SENT), time, events);
-     //if(mf->get_addr() == 0xc0248d80){
-       // printf("Evict Invalidation Response recieved from core %d for address %x\n", mf->get_sid(), mf->get_addr());
-    //}
+     if((mf->get_addr() & (new_addr_type)(~127)) == 0xc0248d80){
+        printf("Evict Invalidation Response recieved from core %d for address %x\n", mf->get_sid(), mf->get_addr());
+    }
 
 	// Invalidate block
 	block->set_status(INVALID, mf->get_access_sector_mask());
