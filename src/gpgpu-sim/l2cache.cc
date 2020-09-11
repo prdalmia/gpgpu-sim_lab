485062541aaa6a2921e8265b01eecdb3c07c6d8a
prdalmia <pdalmia@wisc.edu>
2020-09-10 21:48:27 -0500

    message

--- src/gpgpu-sim/l2cache.cc
+++ src/gpgpu-sim/l2cache.cc
@@ -500,11 +500,11 @@ void memory_sub_partition:: cache_cycle( unsigned cycle )
                                  throw std::runtime_error("You are at a bad place man");
                              }
 
-                     /*
-                         if((mf->get_addr() & (new_addr_type)(~127)) == 0xc0933480){
+                     
+                         if((mf->get_addr() & (new_addr_type)(~127)) == 0xc0903480){
                          printf("Request from core %d for address %x\n", mf->get_sid() ,m_L2cache->get_line_address(mf, cache_index));
                          }
-                     */
+                     
                          m_L2cache->add_waiting_for_ownership(mf, cache_index);
                          m_L2cache->add_ownership_champion(mf, cache_index, get_id());
                          // assert(!(ownership_champion[(mf->get_addr() & ~(new_addr_type)(m_config->m_L2_config.m_line_sz-1))].empty()));
@@ -525,11 +525,11 @@ void memory_sub_partition:: cache_cycle( unsigned cycle )
                                                cluster_id,
                                                mf->get_mem_config() );
                                 mf_flush->set_type(INVALIDATION);
-                       /*
-                            if((mf->get_addr() & (new_addr_type)(~127)) == 0xc0933480){
-                         printf("Invalidation sent to core %d for address %x\n", invalidation_reciever ,m_L2cache->get_line_address(mf, cache_index));
+                       
+                            if((mf->get_addr() & (new_addr_type)(~127)) == 0xc0903480){
+                         printf("Invalidation sent to core %d for address %x where the incoming address is %x\n", invalidation_reciever ,m_L2cache->get_line_address(mf, cache_index), mf->get_addr());
                          }                               
-                        */
+                        
                           m_L2_icnt_queue->push(mf_flush);
                                 mf_flush->set_status(IN_PARTITION_L2_TO_ICNT_QUEUE,gpu_sim_cycle+gpu_tot_sim_cycle);
                                 // L2 cache accepted request
