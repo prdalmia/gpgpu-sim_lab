a6a7a3774255d6cb1a518fc7c26db29c12a890bd
prdalmia <pdalmia@wisc.edu>
2020-09-11 15:57:40 -0500

    message

--- src/gpgpu-sim/gpu-cache.cc
+++ src/gpgpu-sim/gpu-cache.cc
@@ -1892,12 +1892,9 @@ l1_cache::evict(   mem_fetch *mf,
     bool mshr_hit = m_mshrs.probe(mshr_addr);
     std::list<cache_event> events;
     enum cache_request_status status = m_tag_array->probe(block_addr,cache_index,mf);
-    if ((mf->get_addr() & (new_addr_type)(~127) == 0xc09ae800) && mf->get_sid() == 31){
-        printf("cache_index for crash is %d\n", cache_index);
-    }
 
      if ( !mshr_hit && status == MISS ){
-        printf( "address is %x and core id is %d\n", mf->get_addr(), mf->get_sid());
+        printf( "address is %x and core id is %d and cache_index is %d\n", mf->get_addr(), mf->get_sid(), cache_index);
     }
     assert(!(mshr_hit == 0 && status == MISS));
    
