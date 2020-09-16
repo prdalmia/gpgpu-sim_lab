ec553b0da4d8bf99842d9cb9e6d710f13b61ac31
prdalmia <pdalmia@wisc.edu>
2020-09-15 21:29:32 -0500

    Changing pagerank to strong atomicity

--- src/gpgpu-sim/l2cache.cc
+++ src/gpgpu-sim/l2cache.cc
@@ -467,11 +467,13 @@ void memory_sub_partition:: cache_cycle( unsigned cycle )
                      // This needs to be replaced by index?
                      
                       mem_fetch * mf_pending = m_L2cache->get_waiting_for_ownership(mf, cache_index);
-                     
+                     if(mf->get_type() == INVALIDATION_RESPONSE){
+                       printf("Invalidation response recieved from core %d for address %x going to cache_index %d and memory partition %d \n", mf->get_sid(), mf->get_addr(), cache_index, get_id());
+                       }
                      if (mf_pending){
                       mf_pending->set_reply();
                        if(mf_pending->get_type() == INVALIDATION_RESPONSE){
-                       printf("2 Invalidation response recieved from core %d for address %x going to cache_index %d and memory partition %d \n", mf->get_sid(), mf->get_addr(), cache_index, get_id());
+                       printf("2 Invalidation response recieved from core %d for address %x going to cache_index %d and memory partition %d \n", mf_pending->get_sid(), mf_pending->get_addr(), cache_index, get_id());
                        }
                       assert(mf_pending->get_type() != INVALIDATION_RESPONSE);
                       mf_pending->set_status(IN_PARTITION_L2_TO_ICNT_QUEUE,gpu_sim_cycle+gpu_tot_sim_cycle);
