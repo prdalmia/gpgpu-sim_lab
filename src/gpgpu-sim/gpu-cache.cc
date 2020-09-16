baf5909b9719c51693cdf3810f01b28a2cee2303
prdalmia <pdalmia@wisc.edu>
2020-09-16 15:35:39 -0500

    message

--- src/gpgpu-sim/gpu-cache.cc
+++ src/gpgpu-sim/gpu-cache.cc
@@ -2097,11 +2097,11 @@ void l2_cache::add_ownership_champion(mem_fetch* mf, unsigned cache_index, unsig
  if(cache_index == 677 && id == 39){
      printf("330 adding core %d to ownership champion for address %x and its %d\n", mf->get_sid(), mf->get_addr(), mf->isatomic()); 
     }
-*/
+
   if(cache_index == 618 && id == 11){
      printf("adding core %d to ownership champion for address %x and its %d\n", mf->get_sid(), mf->get_addr(), mf->isatomic()); 
     }
-  
+*/  
   
     cache_block_t* block = m_tag_array->get_block(cache_index);
     block->ownership_champion.push_back(std::make_pair(mf->get_sid(), mf->get_addr()));
@@ -2145,14 +2145,14 @@ void l2_cache::remove_from_ownership_champion_queue(unsigned cache_index, unsign
     if(cache_index == 677 && id == 39){
         printf(" 330 removing core %d from ownership champion for address %x where line address is %x\n and %d", block->ownership_champion.front().first, block->ownership_champion.front().second, block->m_tag, where); 
    }
-  */
+  
 
     if(cache_index == 618 && id == 11){
         printf("removing core %d from ownership champion for address %x where line address is %x\n", block->ownership_champion.front().first, block->ownership_champion.front().second, block->m_tag); 
    }
    
     block->ownership_champion.pop_front();
-   
+  */ 
     
 }
 
