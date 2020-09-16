ad0d3ae1da6974b3707182fe6f2a3361cccb1e2a
prdalmia <pdalmia@wisc.edu>
2020-09-15 23:46:11 -0500

    Changing pagerank to strong atomicity

--- src/gpgpu-sim/gpu-cache.cc
+++ src/gpgpu-sim/gpu-cache.cc
@@ -1978,7 +1978,7 @@ unsigned l2_cache::get_ownership_pending_index( mem_fetch *mf, unsigned id)
     else{
         i->second.second ++;
         if((mf->get_addr() & (new_addr_type)(~127)) == 0xc0955d80){
-       printf(" Incrementing Adding cache_index for address %x\n", mf->get_addr());       
+       printf(" Incrementing Adding cache_index for address %x where value is %d\n", mf->get_addr(), i->second.second );       
         }
     }
    }
@@ -1994,14 +1994,13 @@ void l2_cache::remove_ownership_pending_index( mem_fetch *mf, unsigned id)
     assert(requests_in_ownership_queue.count(addr)>0);
     requests_in_ownership_queue[addr].second--;
      if((mf->get_addr() & (new_addr_type)(~127)) == 0xc0955d80){
-       printf(" Decrementing Adding cache_index for address %x\n", mf->get_addr());       
+       printf(" Decrementing Adding cache_index for address %x and value is %d\n", mf->get_addr(), requests_in_ownership_queue[addr].second );       
         }
     if(requests_in_ownership_queue[addr].second == 0){
         
-        if(((mf->get_addr() & (new_addr_type)(~127)) == 0xc0955d80)){
+        if(addr == 0xc0955d80){
          printf("Removing cache_index for address %x as %d and is atomic %d where ID is %d and core ID is %d\n", mf->get_addr(), requests_in_ownership_queue[addr].first, mf->isatomic(), id, mf->get_sid());  
         }
-        
         requests_in_ownership_queue.erase(addr);
     }
 
